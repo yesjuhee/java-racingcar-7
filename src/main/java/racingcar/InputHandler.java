d11b03309ba21c729f4c2d1e6377ab95e9a6130e
yesjuhee <juhee0924@g.skku.edu>
2024-10-25 17:30:53 +0900

    feat: 기능 6 구현 (사용자가 잘못된 값을 입력할 경우 처리)
    1. 입력한 이름의 길이가 0이거나 6이상인 경우
    2. 양수가 아닌 이동 횟수를 입력한 경우

--- src/main/java/racingcar/InputHandler.java
+++ src/main/java/racingcar/InputHandler.java
@@ -1,25 +1,48 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputHandler {
     public static List<String> getCarNames() {
-        // TODO: Input 형식 검증
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        return Arrays.stream(input.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("경주할 자동차 이름을 입력하세요.");
+        }
+
+        List<String> carNames = new ArrayList<>();
+        for (String carName : input.split(",")) {
+            carName = carName.strip();
+            validateCarNameLength(carName);
+            carNames.add(carName);
+        }
+        return carNames;
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException("이름은 한 글자 이상 작성 가능합니다.");
+        }
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("이름은 5글자 이하로 작성 가능합니다.");
+        }
     }
 
     public static int getTotalRaceCount() {
-        // TODO: Input 형식 검증
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
-        return Integer.parseInt(input);
+        int num;
+        try {
+            num = Integer.parseInt(input);
+            if (num <= 0) {
+                throw new IllegalArgumentException("시도할 횟수는 자연수로 입력해주세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 자연수로 입력해주세요.");
+        }
+        return num;
     }
 }
